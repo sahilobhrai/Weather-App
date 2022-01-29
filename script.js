1e9427716489742139288cc9f66ee89190ef28ff
Sahil Obhrai <82256818+sahilobhrai@users.noreply.github.com>
2022-01-29 23:14:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let weather = {
+    "apiKey": "881affd5be2d7ea4b83a1451606ce566",
+        fetchWeather: function (city) {
+          fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+              city +
+              "&units=metric&appid=" +
+              this.apiKey
+          )
+            .then((response) => {
+              if (!response.ok) {
+                alert("No weather found.");
+                throw new Error("No weather found.");
+              }
+              return response.json();
+            })
+            .then((data) => this.displayWeather(data));
+        },
+        displayWeather: function (data) {
+          const { name } = data;
+          const { icon, description } = data.weather[0];
+          const { temp, temp_max, temp_min} = data.main;
+          document.querySelector(".city").innerText = "Weather in " + name;
+          document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+          document.querySelector(".description").innerText = description;
+          document.querySelector(".temp").innerText = temp + "°C";
+          document.querySelector(".temp_min").innerText ="Min Temp: " + temp_min + "°C";
+          document.querySelector(".temp_max").innerText ="Max Temp: " + temp_max + "°C";
+          document.querySelector(".weather").classList.remove("loading");
+        },
+        search: function () {
+          this.fetchWeather(document.querySelector(".search-bar").value);
+        },
+      };
+      
+      document.querySelector(".search button").addEventListener("click", function () {
+        weather.search();
+      });
+      
+      document
+        .querySelector(".search-bar")
+        .addEventListener("keyup", function (event) {
+          if (event.key == "Enter") {
+            weather.search();
+          }
+        });
+   
